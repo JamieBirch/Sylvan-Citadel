6a2b22f53f4393c445e7c25cb7106a71af8d3ab0
JamieBirch <jamie.d.birch@gmail.com>
2023-08-02 15:15:45 +0200

    show time

--- Assets/Calendar.cs
+++ Assets/Calendar.cs
@@ -10,10 +10,11 @@ public class Calendar : MonoBehaviour
     
     public static int day;
     public float countdown;
-    public float dayLength = 60f;
+    public float dayLength;
     
     public Text daysText;
     public Text countdownText;
+    public Text timeText;
     
     // Start is called before the first frame update
     void Start()
@@ -43,7 +44,13 @@ public class Calendar : MonoBehaviour
             NewDay?.Invoke();
         }
 
-        countdownText.text = string.Format("{0:00.00}", countdown);
+        countdownText.text = $"{countdown:00.00}";
         daysText.text = day.ToString();
+
+        int hours = (int)((1 - countdown/dayLength) * 24);
+        TimeSpan time = new TimeSpan(hours, 0, 0);
+        
+        // DateTime dt = new DateTime((long)(dayLength - countdown));
+        timeText.text = $"{time:t}";
     }
 }

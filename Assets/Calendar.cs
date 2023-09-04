b68290299c6619ea5b35dd755a1aaf600417d97f
JamieBirch <jamie.d.birch@gmail.com>
2023-09-04 18:06:35 +0200

    fix the sun

--- Assets/Calendar.cs
+++ Assets/Calendar.cs
@@ -6,7 +6,8 @@ public class Calendar : MonoBehaviour
 {
     public static event Action NewDay;
     
-    public GameObject sun;
+    // public GameObject sun;
+    public Light sun;
     
     // TimeSpan dayStartTime = new TimeSpan(6, 0, 0);
     private int dayStartTime = 6;
@@ -21,41 +22,49 @@ public class Calendar : MonoBehaviour
 
     private bool newDay;
     private Quaternion sunDefaultRotation;
+
+    // private float sunIntensity;
     
     // Start is called before the first frame update
     void Start()
     {
-        sunDefaultRotation = sun.transform.rotation;
+        // sunDefaultRotation = sun.transform.rotation;
         
         // start world time
         countdown = dayLength;
         day = 1;
         
         newDay = true;
+        // sunIntensity = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var sunTransform = sun.transform;
+        // var sunTransform = sun.transform;
 
         countdown -= Time.deltaTime;
         countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
-        sunTransform.Rotate(Time.deltaTime * (270/dayLength),0, 0);
+        // sunTransform.Rotate(Time.deltaTime * (270/dayLength),0, 0);
         
-        int hours = (int)((1 - countdown/dayLength) * 24);
-        TimeSpan time = new TimeSpan(hours, 0, 0);
+        float hours = (1 - countdown/dayLength) * 24;
+        TimeSpan time = new TimeSpan((int)hours, 0, 0);
+        // float sunIntensity = -(Mathf.Cos((float)hours / 4)) + 1;
+        float newSunIntensity = (-(Mathf.Cos(hours / 4)) + 1)/2;
+        // sunIntensity = Mathf.SmoothDamp(sunIntensity, newSunIntensity, , Time.deltaTime);
         
+        sun.intensity = newSunIntensity;
+
         if (countdown <= 0)
         {
             //start new day
             day++;
             countdown = dayLength;
-            sunTransform.rotation = sunDefaultRotation;
+            // sunTransform.rotation = sunDefaultRotation;
             newDay = true;
         }
 
-        if (hours == dayStartTime && newDay)
+        if ((int)hours == dayStartTime && newDay)
         {
             NewDay?.Invoke();
             newDay = false;

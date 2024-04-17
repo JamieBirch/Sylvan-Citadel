0cc77b8206cb0f18481b8c8ea38cf35d6f4fb8a6
JamieBirch <jamie.d.birch@gmail.com>
2024-04-17 17:06:14 +0200

    add missions

---
+++ Assets/ReachPopulationNumberMission.cs
@@ -0,0 +1,14 @@
+﻿class ReachPopulationNumberMission : Mission
+{
+    public int goalNumber;
+    
+    public override bool CheckFinished()
+    {
+        return GameStats.Population >= goalNumber;
+    }
+
+    public override void GiveWording()
+    {
+        wording = "Reach population " + goalNumber;
+    }
+}

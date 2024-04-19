80be3abee93d8f5d597d9ed408728e4a406afed5
JamieBirch <jamie.d.birch@gmail.com>
2024-04-19 15:18:00 +0200

    fix npe

--- Assets/TerrainButton.cs
+++ Assets/TerrainButton.cs
@@ -8,16 +8,33 @@ public class TerrainButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (TileManager.instance.activeTile == null && button.IsInteractable())
-        {
-            button.interactable = false;
-        } else if (TileManager.instance.activeTile.GetComponent<OwnedHex>().GetWoodland() == null)
-        {
-            button.interactable = false;
-        } else if (TileManager.instance.activeTile.GetComponent<OwnedHex>().GetWoodland().getCount() > 0 )
+        if (CheckIfTreesAvailable())
         {
             button.interactable = true;
         }
+        else
+        {
+            button.interactable = false;
+        }
+        
+        
+        
+    }
+
+    private bool CheckIfTreesAvailable()
+    {
+        if (TileManager.instance.activeTile == null)
+        {
+            return false;
+        } else if (TileManager.instance.activeTile.GetComponent<OwnedHex>().GetWoodland() == null)
+        {
+            return false;
+        } else if (TileManager.instance.activeTile.GetComponent<OwnedHex>().GetWoodland().getCount() > 0 )
+        {
+            return true;
+        }
+
+        return false;
     }
 
 }

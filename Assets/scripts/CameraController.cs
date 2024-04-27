f458c797ef635608d304ed360430cc8eef4d29c4
JamieBirch <jamie.d.birch@gmail.com>
2024-04-27 12:24:38 +0200

    cursor camera movement, smooth zoom

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -6,31 +6,33 @@ public class CameraController : MonoBehaviour
     //TODO: screen margin camera movement
     //TODO: camera rotation
     public float panSpeed;
+    public float panBorderThickness;
     public Vector2 panLimit;
 
     public float scrollSpeed;
     public float miny = 20f;
     public float maxy = 120f;
+    private float velocity = 0;
 
     void Update()
     {
         Vector3 pos = transform.position;
         
         //move
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             pos.z += panSpeed * Time.deltaTime;
         }
-        if (Input.GetKey("s"))
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
         {
             pos.z -= panSpeed * Time.deltaTime;
         }
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
             pos.x += panSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
         {
             pos.x -= panSpeed * Time.deltaTime;
         }
@@ -40,7 +42,9 @@ public class CameraController : MonoBehaviour
 
         //zoom
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+        pos.y = Mathf.SmoothDamp(pos.y, pos.y - scroll * scrollSpeed * 100f * Time.deltaTime, ref velocity, Time.deltaTime);
+        // Vector3 targetPosition = target.position + offset;
+        // pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
         pos.y = (Mathf.Clamp(pos.y, miny, maxy));
 
         transform.position = Vector3.Lerp(pos, new Vector3(pos.x, pos.y, pos.z),  Mathf.SmoothStep(0, 1,Time.deltaTime / 100f));
